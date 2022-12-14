e5dac6b253a4dd09e63d6547c53f49a4282e335e
colinzhangg <116466550+colinzhangg@users.noreply.github.com>
2022-12-14 18:48:33 -0500

    update

---
+++ submit.js
@@ -0,0 +1,10 @@
+//get input from user
+
+function getUserInput(){
+    var nameField = document.getElementByClass('name_input');
+    var emailField = document.getElementByClass('email_input');
+}
+
+submitButton.addEventListener('click', function() {
+    alert('You have successfully entered the raffle!');
+})
