9fe338e819ad434141cf0b750400e13bb6181f77
colinzhangg <116466550+colinzhangg@users.noreply.github.com>
2022-12-23 00:01:19 -0500

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,9 +1,9 @@
 //get input from user
 
 function myfunc() {
-    var name = document.getElementById('nameInput');
-    var email = document.getElementById('emailInput');
-    alert("You have successfully entered the raffle!");
+    var name = document.getElementById('nameInput').value;
+    var email = document.getElementById('emailInput').value;
+    alert(name + ", you have successfully entered the raffle!");
 } 
 
 // var submitButton = document.getElementByClass('submitButton');
